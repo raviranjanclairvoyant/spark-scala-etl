1f39ea758d61106c6762af8e3ba1f8c7b6b53464
Ravi Ranjan <Ravi179076@exl-gcxkgd7yyn.dlink>
2023-11-12 11:25:57 +0530

    added a data frame code

--- src/main/scala/org/example/hello.scala
+++ src/main/scala/org/example/hello.scala
@@ -1,5 +1,6 @@
 package org.example
 
+import org.apache.spark.rdd
 import org.apache.spark.sql.SparkSession
 
 object hello{
@@ -7,9 +8,18 @@ object hello{
 
  println("hello scala is ready")
     val spark = SparkSession.builder()
-      .master("yarn")
+      .master("local")
       .appName("My name is ravi")
       .getOrCreate()
+    import spark.implicits._
+
+    val someDF = Seq(
+      (8, "bat"),
+      (64, "mouse"),
+      (-27, "horse")
+    ).toDF("number", "word")
+
+    someDF.show()
 
     println(spark.sparkContext.appName)
   }
